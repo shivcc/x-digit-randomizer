686aa19fe1a7484dba8f93c88cf8f3913fc4c04b
shivcc <shivamchudasama08@gmail.com>
2025-12-09 21:36:03 +0300

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/shivcc/x-digit-randomizer/sw.js', { scope: '/shivcc/x-digit-randomizer/' })})}
