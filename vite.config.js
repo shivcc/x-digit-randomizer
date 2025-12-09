2e2b44f7b128f5303ffc7e2277875d0797d3044d
shivcc <shivamchudasama08@gmail.com>
2025-12-09 21:31:22 +0300

    init

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "shivcc/x-digit-randomizer",
   plugins: [react(), tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
